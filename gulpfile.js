ca2557fe3ab08c344c1546643874d5570fbc120d
AndrLavr Bambambigalo <65401496+bambambigalo@users.noreply.github.com>
2022-07-21 22:08:55 +0300

    pf

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
 const include = require('gulp-file-include');
-const htmlmin = require('gulp-htmlmin');
+// const htmlmin = require('gulp-htmlmin');
+var imagemin = require("gulp-webp");
+// var svgstore = require("gulp-svgstore");
 const del = require('del');
 const sync = require('browser-sync').create();
 
@@ -35,7 +37,29 @@ function serve () {
     watch('./src/**.html', series(html)).on('change', sync.reload)
     watch('./src/scss/**.scss', series(scss)).on('change', sync.reload)
 }
-
+/* Минифицирует изображения*/
+gulp.task("images", function() {
+    return gulp.src("./src/image/**/*.{png,jpg,svg}")
+      .pipe(imagemin([    /* imagemin сам по себе содержит в себе множество плагинов (работа с png,svg,jpg и тд) */
+        imagemin.optipng({optimizationLevel: 3}),  /* 1 - максимальное сжатие, 3 - безопасное сжатие, 10 - без сжатия */
+        imagemin.jpegtran({progressive: true}),    /* прогрессивная загрузка jpg (сначала пиксельная, позже проявляется) */
+        imagemin.svgo()   /*Минификация svg от лишних тегов*/
+        ]))
+      .pipe(gulp.dest("dist/img"));
+  });
+/* Сборка спрайта */
+// gulp.task("sprite", function() {
+//     return gulp.src("./build/img/inline-icons/*.svg")
+//       .pipe(svgstore({    /* Делает спрайт из SVG-файлов */
+//         inLineSvg: true
+//       }))
+//        .pipe(imagemin([
+//         imagemin.svgo()
+//         ]))
+//       .pipe(rename("sprite.svg"))
+//       .pipe(gulp.dest("./build/img"))
+//       .pipe(server.stream());
+//   });
 exports.build = series(clear, scss, html)
 exports.serve = series(clear, scss, html, serve)
 exports.clear = clear;
